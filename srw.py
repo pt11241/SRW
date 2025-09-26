Fixed averaging, graph

srw.py
@@ -38,33 +38,41 @@ class Quadratic_Irrationals:
         return res_q
 
     @staticmethod
-    def med(array : np.array, step : int) -> np.array:
-        part = len(array) // step
-        return [np.mean(array[i * step: (i + 1) * step]) for i in range(part)]
-   
+    def med(array: np.array) -> np.array:
+        size = array.size
+        result = []
+        for D in range(1, size+1):  
+            numerator = np.nansum(array[1:D])  
+            denominator = D - np.sqrt(D)
+            result.append(numerator / denominator)
+        return np.array(result)
 
 
 def main():
     k = Quadratic_Irrationals(4, 6, 22)
     print(k.periodic_counted_fraction(20))
     p0,Q0 = 0, 1
-    D = np.arange(1, 40000)
+    D = np.arange(1, 1000)
     indx = np.sqrt(D) % 1 != 0
     D = D[indx]
     lens = np.array([Quadratic_Irrationals(p0, Q0, i).period() for i in D])
-    print(len(D))
 
-    x_D = Quadratic_Irrationals.med(D, 500)
-    Lmed = Quadratic_Irrationals.med(lens, 500)
+    # x_D = Quadratic_Irrationals.med(D)
+    Lmed = Quadratic_Irrationals.med(lens)
     print(lens[:10])
     print("AAAAAAAAAAAAAAa")
-    print(Lmed)
+    print(Lmed[:10])
 
     
-    DmedGraph = plt.plot(x_D, Lmed, color='b', alpha=1)
-    lens_D = plt.plot(D, lens, color='g', alpha=0.7)
+    plt.figure(figsize=(14, 7))
 
-    # plt.setp(lens_D[0])
+    DmedGraph = plt.plot(D, Lmed, color='b', alpha=1, label='Усреднённые значения', linewidth=3)
+    lens_D = plt.plot(D, lens, 'o', color='g', alpha=0.3, label='Периоды')
+    # plt.plot(D, Lmed, 'o-', color='b', alpha=0.8, label='Усреднённые значения')
+    # plt.plot(D, lens, '-', color='g', alpha=0.6, label='Периоды')
+    plt.legend()
+
+    plt.title("Длины периодов и их усреднение")
     plt.grid()
     plt.xlabel("D")
     plt.ylabel("L(D)")